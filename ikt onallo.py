1e6976489dd2460229c376636e9692b3f2b24887
KőszegiTamás <74605307+creppzguy@users.noreply.github.com>
2021-11-10 09:29:28 +0100

    Add files via upload

--- ikt onallo.py
+++ ikt onallo.py
@@ -2,5 +2,19 @@ import math
 a=float(input("Írd ide az első számot: "))
 b=float(input("Írd ide a második számot: "))
 c=float(input("Írd ide a harmadik számot: "))
-x=a*a-4*b*c
+d=b*b-4*a*c
+if d<0:
+    print("Nincs ilyen megoldás")
+
+else:
+    if d=0:
+        print("Egyetlen megoldás van: ",-b/(2*a))
+    else:
+        
+    
+    
+    
+
+
+
 
