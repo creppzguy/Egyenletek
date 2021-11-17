a50ba033dc1fdc398a815af847f6c4282ab5773f
KőszegiTamás <74605307+creppzguy@users.noreply.github.com>
2021-11-17 09:17:32 +0100

    Add files via upload

--- ikt onallo.py
+++ ikt onallo.py
@@ -7,14 +7,14 @@ if d<0:
     print("Nincs ilyen megoldás")
 
 else:
-    if d=0:
-        print("Egyetlen megoldás van: ",-b/(2*a))
+    if d==0:
+        d1=(-b+math.sqrt(b**2-4*a*c))/2*a
+        print("Egyetlen megoldás van: ", d1)
     else:
-        
+        if d>0:
+           d2=(-b + math.sqrt(d)) / (2*a)
+           d3=(-b - math.sqrt(d)) / (2*a)
+           print("Kettő megoldás van megoldás: ", d2, "vagy", d3)
+
+
     
-    
-    
-
-
-
-
